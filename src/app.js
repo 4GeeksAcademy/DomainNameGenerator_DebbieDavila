97092c3e3cacdab79be18423122344de0aa8daf1
Debbie Dávila <146867711+DebbieDavilaDev@users.noreply.github.com>
2024-02-14 21:48:19 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronouns = ["my", "your", "their"];
+  const adjs = ["fantabulous", "amazing", "great"];
+  const nouns = ["artiste", "designer", "website"];
+  const verbs = ["smiling", "laughing", "running"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let verb of verbs) {
+          console.log(pronoun, adj, noun, verb);
+          document.getElementById(
+            "results"
+          ).innerHTML += `<li>${pronoun}${adj}${noun}${verb}</li>`;
+        }
+      }
+    }
+  }
 };
